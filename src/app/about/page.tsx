1896da0fae90d479048e6240e342a74bf61068f2
oleh <oleh.abanin@yolo.com>
2024-11-20 18:03:06 +0200

    fix: eslint

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import NextLink from 'next/link';
-import ProTip from '../../components/ProTip';
-import Copyright from '../../components/Copyright';
 
 export default function About() {
     return (
