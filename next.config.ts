7d45b120cb30c3cc8a4e1499ca5294da434fb825
oleh <oleh.abanin@yolo.com>
2024-12-10 17:01:39 +0200

    fix: asset prefic

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? "/shutdown-timer/" : "",
-  basePath: isProd ? "/shutdown-timer" : "",
+  // assetPrefix: isProd ? "/shutdown-timer/" : "",
+  // basePath: isProd ? "/shutdown-timer" : "",
   output: "export",
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
