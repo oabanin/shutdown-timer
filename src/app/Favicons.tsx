a5d4acbf89c38c948d087cbc65c21487d69deb78
oleh <oleh.abanin@yolo.com>
2024-12-12 09:32:22 +0200

    fix: update favicon

--- src/app/Favicons.tsx
+++ src/app/Favicons.tsx
@@ -6,18 +6,18 @@ export const Favicons = () => {
       <link
         rel="icon"
         type="image/png"
-        href="/favicon/favicon-96x96.png"
+        href="/favicon-96x96.png"
         sizes="96x96"
       />
-      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <meta name="apple-mobile-web-app-title" content="Shutdown timer" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 };
